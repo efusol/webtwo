d90bf6fc0ae5f585a849b6d1243f1a3c286c6c1f
jingjeong <jingi745@naver.com>
2024-07-17 15:44:02 +0900

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,14 @@ import HomeView from './views/HomeView';
 import JoinView from './views/JoinView';
 import LoginView from './views/LoginView';
 import ScrollToTop from './ScrollToTop';
+import BaseballView from './views/BaseBallView';
+import BaseBallDetailView from './views/BaseBallDetailView';
+import BaseballHomeView from './views/BaseballHomeView';
+import BaseBallHistoryView from './views/BaseBallHistoryView';
+import BoardListView from './views/BoardListView';
+import BoardWriteView from './views/BoardWriteView';
+import BoardDetailView from './views/BoardDetailView'; 
+import BoardModifyView from './views/BoardModifyView';
 
 const App = () => {
   return (
@@ -13,8 +21,16 @@ const App = () => {
       <Routes>
         <Route path='/' element={<Layout />}>
           <Route index element={<HomeView />} />
+          <Route path='/baseballhome' element={<BaseballHomeView />} />
+          <Route path='/baseball' element={<BaseballView />} />
+          <Route path='/player/:id' element={<BaseBallDetailView />} />
+          <Route path='/baseballhistory' element={<BaseBallHistoryView />} />
           <Route path='/join' element={<JoinView />} />
           <Route path='/login' element={<LoginView />} />
+          <Route path='/board' element={<BoardListView />} />
+          <Route path='/boardwrite' element={<BoardWriteView />} />
+          <Route path='/boardDetail/:subject' element={<BoardDetailView />} />
+          <Route path='/boardModify/:subject' element={<BoardModifyView />} />
         </Route>
       </Routes>
     </BrowserRouter>
--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from  '@/store/user.js'
+import playerReducer from '@/store/player'
+import boardReducer from '@/store/board'
 
 const store = configureStore({
   reducer: {
-    users: userReducer
+    players: playerReducer,
+    users: userReducer,
+    boards : boardReducer
   }
 })
 

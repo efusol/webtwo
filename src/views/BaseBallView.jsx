2e0c4dbe1c58be8a89cd511bb479d9a6ed169b67
jingjeong <jingi745@naver.com>
2024-07-19 11:15:45 +0900

    Add files via upload

--- src/views/BaseBallView.jsx
+++ src/views/BaseBallView.jsx
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 import PlayerCategory from '@/components/baseball/PlayerCategory';
 import PlayerSection from '@/components/baseball/PlayerSection';
 import { useSelector } from 'react-redux';
+import PlayerBanner from '@/components/baseball/PlayerBanner';
 
 
 
@@ -11,6 +12,7 @@ const BaseBallView = () => {
     const category = useSelector(state=>state.players.category)
     return (
         <div>
+            <PlayerBanner />
             <PlayerCategory title={category} />
             <PlayerSection title={category} />
         </div>
